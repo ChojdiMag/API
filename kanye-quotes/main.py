fa948d78618d14600077dac5d8902aa85b57ab25
Magdalena Chojdacka <97465056+ChojdiMag@users.noreply.github.com>
2023-11-07 18:54:52 +0100

    Kanye's quotes

---
+++ kanye-quotes/main.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "chojdackam@gmail.pl"
+MY_PASSWORD = "1234432112344321"
+MY_LAT = 53.199089
+MY_LONG = 23.207279
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smpt.gmail.com_")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+        )
